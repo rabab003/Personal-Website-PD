e1821be45121970ac3b99f65a5ef390e1b34054f
rabab003 <bintaliraq4@gmail.com>
2025-02-12 17:10:39 +0300

    5:10 pm 12 feb

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 
-import './App.css'
+import './App.css';
+import './Components/LoadingScreen';
+import './index.css'
+import { LoadingScreen } from './Components/LoadingScreen';
+import { useState } from 'react';
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
 
   return (
   <>
-  <h1 className='uppercase'>
-    hello world</h1></>
+   {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />} 
+    
+    
+    </>
   )
 }
 
--- src/Components/LoadingScreen.jsx
+++ src/Components/LoadingScreen.jsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react"
+
 export const LoadingScreen = () => {
+    const [text , setText] =useState("");
+    const fullText = "<Hello world/>";
+
+    useEffect(()=> {
+        let index =0 ;
+        const interval = setInterval(()=>{
+            setText(fullText.substring(0, index));
+            index++;
+
+            if(index > fullText.length){
+                clearInterval(interval)
+
+            }
+    }, 100);
+    return () => clearInterval(interval);
+    });
+
     return (
         <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
 
             <div className="mb-4 text-4xl font-mono font-bold">hello world 
-                <span className="animate-ping ml-1">|</span>
+              {text}  <span className="animate-ping ml-1">|</span>
             </div>
 
             <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
