d11a41509fdb66bb09b262f1b46f5ba746621755
rabab003 <bintaliraq4@gmail.com>
2025-02-13 15:21:40 +0300

    created the Nav

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import './Components/LoadingScreen';
 import './index.css'
 import { LoadingScreen } from './Components/LoadingScreen';
 import { useState } from 'react';
+import { Navbar } from './Components/Navbar';
+
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -11,6 +13,10 @@ function App() {
   return (
   <>
    {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />} 
+
+    <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100": "opacity-0"} bg-black text-gray-100`}>
+<Navbar/>
+    </div>
     
     
     </>
--- src/Components/LoadingScreen.jsx
+++ src/Components/LoadingScreen.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-export const LoadingScreen = () => {
+export const LoadingScreen = ({oncomplete}) => {
     const [text , setText] =useState("");
     const fullText = "<Hello world/>";
 
@@ -12,23 +12,25 @@ export const LoadingScreen = () => {
 
             if(index > fullText.length){
                 clearInterval(interval)
-
+                setTimeout(()=>{
+                    oncomplete();
+                },1000);
             }
     }, 100);
+
     return () => clearInterval(interval);
-    });
+    } ,[oncomplete]);
 
     return (
         <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
 
-            <div className="mb-4 text-4xl font-mono font-bold">hello world 
-              {text}  <span className="animate-ping ml-1">|</span>
-            </div>
+<div className="mb-4 text-4xl font-mono font-bold">
+        {text} <span className="animate-blink ml-1"> | </span>
+      </div>
 
             <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
-                <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
-
-            </div>
+        <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
+      </div>
 
         </div>
     )
