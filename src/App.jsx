85706ba1c20e290818c8d6f53d9958bd233b66d3
rabab003 <bintaliraq4@gmail.com>
2025-02-16 17:39:16 +0300

    16 feb

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { Navbar } from './Components/Navbar';
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
   <>
@@ -15,7 +16,7 @@ function App() {
 
     <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100": "opacity-0"} bg-black text-gray-100`}>
     </div>
-    <Navbar/>
+    <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
 
     
     </>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,10 @@
-export const Navbar = () => {
+import { useEffect } from "react"
+
+
+export const Navbar = ({menuOpen,setMenuOpen}) => {
+    useEffect(()=>{
+        document.body.style.overflow = menuOpen ? "hidden" : "";
+    }, [menuOpen]);
     return (
     <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-b border-white shadow-lg">
         <div className="max-w-5xl max-auto px-4">
