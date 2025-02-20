9be72a5f7b1a4b625f075b414cd7e8f5ce2e022a
rabab003 <bintaliraq4@gmail.com>
2025-02-20 10:15:36 +0300

    nav done, home started 20 feb

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
 import './App.css';
-import './Components/LoadingScreen';
+// import './Components/LoadingScreen';
 import './index.css'
 import { LoadingScreen } from './Components/LoadingScreen';
 import { useState } from 'react';
 import { Navbar } from './Components/Navbar';
+import MobileMenu from './Components/MobileMenu'
+// import Home from "./Components/Sections/Home"
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -17,6 +19,11 @@ function App() {
     <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100": "opacity-0"} bg-black text-gray-100`}>
     </div>
     <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+    <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+
+    {/* <Home/> */}
+
+
 
     
     </>
