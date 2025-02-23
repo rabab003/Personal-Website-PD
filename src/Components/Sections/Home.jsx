39690abc6aae354b83e8fb1263bdd76413222bfa
rabab003 <bintaliraq4@gmail.com>
2025-02-23 21:00:14 +0300

    revealon scroll components

--- src/Components/Sections/Home.jsx
+++ src/Components/Sections/Home.jsx
@@ -1,16 +1,3 @@
-// export const Home = ()=> {
-//     return (
-//         <section id="home" className="min-h-screen flex items-center justify-center relative" >
-
-//             <div className="text-center z-10 px-4">
-//                 <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to bg-purple-600 leading-right text-transparent bg-clip-text">Hi i'm rabab</h1>
-//             </div>
-
-//         </section>
-//     )
-// } 
-
-
 import { RevealOnScroll } from "../RevealOnScroll";
 
 export const Home = () => {
