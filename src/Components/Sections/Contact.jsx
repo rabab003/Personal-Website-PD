4deaa100852a1bf7ce3bab856e7fa339da8ae6c9
rabab003 <bintaliraq4@gmail.com>
2025-02-25 07:17:42 +0300

    25 feb

--- src/Components/Sections/Contact.jsx
+++ src/Components/Sections/Contact.jsx
@@ -1,4 +1,5 @@
-import { RevealOnScroll } from "../RevealOnScroll"
+import { RevealOnScroll } from "../RevealOnScroll";
+import emailjs from 'emailjs-com'
 
 export const Contact =() =>{
     return (
@@ -28,7 +29,7 @@ export const Contact =() =>{
 
                         <button type="submit" className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]">Send Message</button>
 
-                    </form>
+                    </form>  
 
                 </div>
 
